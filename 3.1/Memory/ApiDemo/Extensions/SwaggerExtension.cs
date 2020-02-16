f80eac242ba4e84ed2fdc051d9f078a6a70ff6f7
imstrive <longtaosu@sina.com>
2020-02-16 21:41:56 +0800

    添加IMemoryCache

---
+++ 3.1/Memory/ApiDemo/Extensions/SwaggerExtension.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDemo.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static IServiceCollection AddSwaggerExt(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("V1", new OpenApiInfo
+                {
+                    Title = "LTS's Api",
+                    Version = "V1"
+                });
+
+                var security = new Dictionary<string, IEnumerable<string>> { { "LTS", new string[] { } }, };
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT授权(数据将在请求头中进行传输) 直接在下框中输入Bearer {token}",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                { new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference()
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    }, Array.Empty<string>() }
+                });
+            });
+        }
+
+        public static IApplicationBuilder UseSwaggerExt(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/V1/swagger.json", "Api 接口");
+            });
+
+            return app;
+        }
+    }
+}
